9047869d5a1328f62e723728e82e14784c6bf067
Ast <vitor.orsin@estudante.ufscar.br>
2023-08-23 23:33:16 -0300

    Few QoL changes

--- Game Jam Brackeys/Assets/Scripts/Sling.cs
+++ Game Jam Brackeys/Assets/Scripts/Sling.cs
@@ -2,6 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+
+/*
+    Sling Mechanic
+
+    TODO
+    * limit sling angle
+    * playtest and adjust sling force
+    * make collision interactions
+    * probally should make the click detection a little better
+    * visual indicators
+*/
 
 public class Sling : MonoBehaviour
 {
@@ -12,9 +24,15 @@ public class Sling : MonoBehaviour
     private Vector3 slingDirection;
     private Ray mouseCast;
     private Plane plane;
-        void Start()
+
+    private GameObject slingHUD;
+
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
+        slingHUD = transform.GetChild(0).gameObject;
+        //slingHUD.transform.rotation = Quaternion.identity;
+        slingHUD.SetActive(false);
         plane = new Plane(Vector3.forward, transform.position);
     }
 
@@ -28,6 +46,9 @@ public class Sling : MonoBehaviour
                 Vector3 hit = mouseCast.GetPoint(enter);
                 Vector3 playerPos = plane.ClosestPointOnPlane(transform.position);
                 slingDirection = new Vector3(hit.x - playerPos.x, hit.y - playerPos.y, 0);
+
+                // Quaternion rot = Quaternion.LookRotation(slingDirection);
+                slingHUD.transform.rotation = new Quaternion(slingDirection.x, slingDirection.y, 0, 0);
             }
         }
         if(Input.GetMouseButtonDown(0)){
@@ -35,13 +56,17 @@ public class Sling : MonoBehaviour
             RaycastHit hit;
             if(Physics.Raycast(mouseCast, out hit) && hit.transform == transform){
                 Debug.Log("Achei o jogador");
+                rb.velocity = Vector3.zero;
+                slingHUD.SetActive(true);
                 isHoldingSling = true;
             }
         }
         if(Input.GetMouseButtonUp(0)&& isHoldingSling){
+            slingHUD.SetActive(false);
+            slingHUD.transform.rotation = Quaternion.identity;
             isHoldingSling = false;
-            rb.AddForce(-slingDirection.normalized * slingForce, ForceMode.Impulse);
-            rb.AddForce(slingDirection.normalized * slingForce * 0.8f, ForceMode.Impulse);
+            rb.AddForce(-slingDirection * slingForce, ForceMode.Impulse);
+            rb.AddForce(slingDirection * slingForce * 0.8f, ForceMode.Impulse);
         }
     }
 }
