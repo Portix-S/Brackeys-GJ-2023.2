50937da052176c6e4b182064741e1c68db5787c9
lksalto <saltolks@gmail.com>
2023-08-25 20:35:25 -0300

    Speedrun e Endgame

--- Game Jam Brackeys/Assets/Scripts/FinishLine.cs
+++ Game Jam Brackeys/Assets/Scripts/FinishLine.cs
@@ -1,24 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] Transform playerTransform;
     [SerializeField] Scrollbar sb;
+    [SerializeField] GameObject endScreen;
+    [SerializeField] TextMeshProUGUI scoreFinal;
+    [SerializeField] TextMeshProUGUI timeText;
+    float fallTime = 0;
     float dif;
     float initialDist;
-    // Start is called before the first frame update
+    bool isFalling = true;
+
     void Start()
     {
         initialDist = playerTransform.position.y - transform.position.y;
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        Scroll();
+        CalculateTime();
+    }
+
+    public void Scroll()
     {
         dif = playerTransform.position.y - transform.position.y;
         sb.value = dif / initialDist;
+    }
+
+    public void CalculateTime()
+    {
+        if (isFalling)
+        {
+            fallTime += Time.deltaTime;
+            timeText.text = fallTime.ToString();
+        }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isFalling = false;
+            timeText.transform.parent.gameObject.SetActive(false);
+            scoreFinal.text = timeText.text;
+            endScreen.SetActive(true);
+        }
+        
+        
     }
 }
