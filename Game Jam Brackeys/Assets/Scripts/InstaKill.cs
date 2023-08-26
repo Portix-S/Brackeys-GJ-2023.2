6460af0aa95ae13672b3c9b29bbbd9b5db4b2a8e
lksalto <saltolks@gmail.com>
2023-08-26 17:32:55 -0300

    Imp, Lapide e Toupeira 2

--- Game Jam Brackeys/Assets/Scripts/InstaKill.cs
+++ Game Jam Brackeys/Assets/Scripts/InstaKill.cs
@@ -8,7 +8,7 @@ public class InstaKill : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            Destroy(collision.gameObject.transform.root.gameObject);
+            collision.GetComponent<PlayerHealth>().Die();
         }
     }
 }
